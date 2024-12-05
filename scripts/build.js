1eab2ca9ba00f626323c6552b25c6104d498139d
Leon Ossowski <leon.ossowski@iem.thm.de>
2024-12-05 20:23:12 +0100

    add web files

--- scripts/build.js
+++ scripts/build.js
@@ -28,7 +28,7 @@ createBuilder(
     },
   ],
   async (outfiles) => {
-    const files = await getFiles('static')
+    const files = await getFiles('dist/web', 'static')
     await createFxmanifest({
       client_scripts: [outfiles.client],
       server_scripts: [outfiles.server],

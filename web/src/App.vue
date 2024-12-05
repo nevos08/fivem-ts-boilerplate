8c64843d4c6fd214944b4c3880dab246d11385d6
Leon Ossowski <leon.ossowski@iem.thm.de>
2024-12-05 20:43:10 +0100

    init vue

---
+++ web/src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h1>Hello from Vue!</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  font-weight: bold;
+  font-family: sans-serif;
+}
+</style>
